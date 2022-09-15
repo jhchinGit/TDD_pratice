1a6a07bed4004a5b82e52ba0a8fbeef0cf90d5bb
JHSW <50164113+jhchinGit@users.noreply.github.com>
2022-09-15 22:02:38 +0800

    Update ReverseInteger.cs

--- TDD_Pracitce/ReverseInteger.cs
+++ TDD_Pracitce/ReverseInteger.cs
@@ -6,7 +6,7 @@ namespace TDD_Pracitce
     {
         public static int Reverse(int x)
         {
-            if (isBiggerThan32Bit(x) || x == int.MaxValue || x == int.MinValue)
+            if (x == int.MaxValue || x == int.MinValue)
                 return 0;
 
             bool isPositiveNumber = x >= 0;
@@ -29,26 +29,18 @@ namespace TDD_Pracitce
                     string exchangeTail = numberString.Substring(numberString.Length - index, 1);
                     string tail = numberString.Substring(numberString.Length - index + 1, index - 1);
                     numberString = head + exchangeTail + middle + exchangeHead + tail;
-
-                    if (isBiggerThan32Bit(x))
-                        return 0;
                 }
 
                 var result = long.Parse(numberString);
                 result = isPositiveNumber ? result : result * -1;
 
-                if (isBiggerThan32Bit(result))
+                if (!int.TryParse(result.ToString(), out var longResult))
                     return 0;
 
                 return (int)result;
             }
 
             return x;
-        }
-
-        private static bool isBiggerThan32Bit(long number)
-        {
-            return !int.TryParse(number.ToString(), out var result);
         }
     }
 }
