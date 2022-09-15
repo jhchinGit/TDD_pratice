bec73618c51890dc7ec33c4486a4f41e8726647d
JHSW <50164113+jhchinGit@users.noreply.github.com>
2022-09-15 21:53:05 +0800

    Resolve Reverse Integer

--- TDD_Pracitce/ReverseInteger.cs
+++ TDD_Pracitce/ReverseInteger.cs
@@ -4,36 +4,51 @@ namespace TDD_Pracitce
 {
     public static class ReverseInteger
     {
-        public static int Reverse(int number)
+        public static int Reverse(int x)
         {
-            bool isPositiveNumber = number >= 0;
-            var positiveNumber = isPositiveNumber ? number : number * -1;
+            if (isBiggerThan32Bit(x) || x == int.MaxValue || x == int.MinValue)
+                return 0;
+
+            bool isPositiveNumber = x >= 0;
+            var positiveNumber = isPositiveNumber ? x : x * -1;
             var numberString = positiveNumber.ToString();
 
             if (numberString.Length > 1)
             {
                 int loopCount = (int)Math.Floor((decimal)numberString.Length / 2);
-                int middleCount = (int)Math.Ceiling((decimal)numberString.Length / 2);
                 for (int index = 1; index <= loopCount; index++)
                 {
                     string head = numberString.Substring(0, index - 1);
                     string exchangeHead = numberString.Substring(index - 1, 1);
 
-                    int middleLength = numberString.Length - middleCount - index + (numberString.Length % 2 == 0 ? 1 : 0);
+                    int backLength = numberString.Length - (numberString.Length - index);
+                    int headLength = numberString.Length - index;
+                    int middleLength = headLength - backLength;
                     string middle = numberString.Substring(index, middleLength);
-                    middleCount += 1;
+
                     string exchangeTail = numberString.Substring(numberString.Length - index, 1);
-                    string tail = numberString.Substring(numberString.Length - 1, index - 1);
+                    string tail = numberString.Substring(numberString.Length - index + 1, index - 1);
                     numberString = head + exchangeTail + middle + exchangeHead + tail;
+
+                    if (isBiggerThan32Bit(x))
+                        return 0;
                 }
 
-                var result = int.Parse(numberString);
-                return isPositiveNumber ? result : result * -1;
+                var result = long.Parse(numberString);
+                result = isPositiveNumber ? result : result * -1;
+
+                if (isBiggerThan32Bit(result))
+                    return 0;
+
+                return (int)result;
             }
-            else
-            {
-                return number;
-            }
+
+            return x;
+        }
+
+        private static bool isBiggerThan32Bit(long number)
+        {
+            return !int.TryParse(number.ToString(), out var result);
         }
     }
 }
--- TDD_Practice.Test/UnitTest1.cs
+++ TDD_Practice.Test/UnitTest1.cs
@@ -5,11 +5,6 @@ namespace TDD_Practice.Test
 {
     public class ReverseIntegerTest
     {
-        //[SetUp]
-        //public void Setup()
-        //{
-        //}
-
         [Test]
         public void GIVEN_number___WHEN_reverse___THEN_reversed_number()
         {
@@ -21,6 +16,11 @@ namespace TDD_Practice.Test
             ReverseNumberEqual(123, 321);
             ReverseNumberEqual(-123, -321);
             ReverseNumberEqual(1234, 4321);
+            ReverseNumberEqual(123456, 654321);
+            ReverseNumberEqual(-123456, -654321);
+            ReverseNumberEqual(123456789, 987654321);
+            ReverseNumberEqual(1534236469, 0);
+            ReverseNumberEqual(-2147483648, 0);
         }
 
         private static void ReverseNumberEqual(int targetNumber, int expectedNumber)
