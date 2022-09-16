38e64d08df0faa72be0b9fca825572a114c179f2
JHSW <50164113+jhchinGit@users.noreply.github.com>
2022-09-16 15:18:44 +0800

    Resolved remove duplicate

---
+++ TDD_Pracitce/RemoveDuplicateHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace TDD_Practice
+{
+    public class RemoveDuplicateHelper
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int count = 0;
+
+            for (int index = 0; index < nums.Length; index++)
+            {
+                bool isDuplicated = false;
+                for (int x = index + 1; x < nums.Length; x++)
+                {
+                    if (nums[x] == nums[index])
+                    {
+                        isDuplicated = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicated)
+                    nums[count++] = nums[index];
+            }
+
+            return count;
+        }
+    }
+}
---
+++ TDD_Practice.Test/RemoveDuplicatesFromSortedArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace TDD_Practice.Test
+{
+    internal class RemoveDuplicatesFromSortedArrayTest
+    {
+        [Test]
+        public void GIVEN_integer_array___THEN_remove_duplicate_number()
+        {
+            RemoveDuplicateCountEqual(GenerateArray(1, 2, 2), 2);
+            RemoveDuplicateCountEqual(GenerateArray(1), 1);
+            RemoveDuplicateCountEqual(GenerateArray(1, 2, 3, 4, 3, 3, 3, 4), 4);
+        }
+
+        private static int[] GenerateArray(params int[] values)
+        {
+            return values;
+        }
+
+        private static void RemoveDuplicateCountEqual(int[] nums, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, RemoveDuplicateHelper.RemoveDuplicates(nums));
+        }
+    }
+}
