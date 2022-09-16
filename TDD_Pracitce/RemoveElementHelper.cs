43af0bb90e9eef1f0ea0972b1cbe6f2cf2740227
JHSW <50164113+jhchinGit@users.noreply.github.com>
2022-09-16 09:04:28 +0800

    Remove Element

---
+++ TDD_Pracitce/RemoveElementHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace TDD_Practice
+{
+    public class RemoveElementHelper
+    {
+        public static int RemoveElement(ref int[] arrayToRemove, int valueToRemove)
+        {
+            int count = 0, newIndexCount = 0;
+            int[] newArray = new int[arrayToRemove.Length];
+            for (int index = 0; index < arrayToRemove.Length; index++)
+                if (arrayToRemove[index] != valueToRemove)
+                {
+                    newArray[newIndexCount++] = arrayToRemove[index];
+                    count++;
+                }
+
+            int swapIndex = 0;
+            for (int index = 0; index < arrayToRemove.Length; index++)
+            {
+                for (int x = index, y = 0; y < arrayToRemove.Length; y++)
+                {
+
+                }
+            }
+
+            arrayToRemove = newArray;
+            return count;
+        }
+    }
+}
