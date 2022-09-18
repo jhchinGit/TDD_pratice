c504f26f3604ed6c83fd2d9b8f270a73fb86f1ea
JHSW <50164113+jhchinGit@users.noreply.github.com>
2022-09-18 15:38:23 +0800

    Resolved Roman to Integer

---
+++ TDD_Pracitce/RomanToIntegerHelper.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace TDD_Practice
+{
+    public class RomanToIntegerHelper
+    {
+        private static readonly Dictionary<char, int> RomanDictioary = new()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public static int RomanToInt(string roman)
+        {
+            int total = 0;
+
+            int previousNumber = 0;
+            for (int index = 0; index < roman.Length; index++)
+            {
+                int currentNumber = RomanDictioary[roman[index]];
+
+                if (previousNumber >= currentNumber)
+                {
+                    if (IsThisLastNumber(roman, index))
+                    {
+                        total += previousNumber + currentNumber;
+                        previousNumber = 0;
+                    }
+                    else
+                    {
+                        total += previousNumber;
+                        previousNumber = currentNumber;
+                    }
+                }
+                else if (previousNumber != 0 && previousNumber < currentNumber)
+                {
+                    total += currentNumber - previousNumber;
+                    previousNumber = 0;
+                }
+                else if (IsThisLastNumber(roman, index))
+                {
+                    total += currentNumber;
+                }
+                else
+                {
+                    previousNumber = currentNumber;
+                }
+            }
+
+            return total;
+        }
+
+        private static bool IsThisLastNumber(string roman, int index)
+        {
+            return index + 1 == roman.Length;
+        }
+    }
+}
---
+++ TDD_Practice.Test/RomanToIntegerTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace TDD_Practice.Test
+{
+    internal class RomanToIntegerTest
+    {
+        [Test]
+        public void GIVEN_roman_words___WHEN_roman_to_arabic___THEN_equal_value_with_arabic()
+        {
+            ValueEqualToRomanCharacters("I", 1);
+            ValueEqualToRomanCharacters("V", 5);
+            ValueEqualToRomanCharacters("X", 10);
+            ValueEqualToRomanCharacters("L", 50);
+            ValueEqualToRomanCharacters("C", 100);
+            ValueEqualToRomanCharacters("D", 500);
+            ValueEqualToRomanCharacters("M", 1000);
+            ValueEqualToRomanCharacters("III", 3);
+            ValueEqualToRomanCharacters("IV", 4);
+            ValueEqualToRomanCharacters("VI", 6);
+            ValueEqualToRomanCharacters("LVIII", 58);
+            ValueEqualToRomanCharacters("XIX", 19);
+            ValueEqualToRomanCharacters("MCMXCIV", 1994);
+            ValueEqualToRomanCharacters("MCMXCVI", 1996);
+        }
+
+        private static void ValueEqualToRomanCharacters(string roman, int number)
+        {
+            Assert.AreEqual(number, RomanToIntegerHelper.RomanToInt(roman));
+        }
+    }
+}
