7038d44bc63acd4eb753926bfab4a32dbbf3bfc6
JHSW <50164113+jhchinGit@users.noreply.github.com>
2022-09-18 23:03:14 +0800

    Update

---
+++ TDD_Pracitce/ThreeSumHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TDD_Practice
+{
+    public class ThreeSumHelper
+    {
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            var uniqueResult = new Dictionary<string, List<int>>();
+
+            int[] sortedNums = nums.OrderBy(i => i).ToArray();
+
+            for (int index = 0; index < sortedNums.Length; index++)
+                for (int x = index + 1; x < sortedNums.Length; x++)
+                    for (int y = x + 1; y < sortedNums.Length; y++)
+                    {
+                        var key = $"{sortedNums[index]},{sortedNums[x]},{sortedNums[y]}";
+                        if (!uniqueResult.ContainsKey(key) && sortedNums[index] + sortedNums[x] + sortedNums[y] == 0)
+                        {
+                            uniqueResult.TryAdd(key, new List<int>
+                            {
+                                sortedNums[index],
+                                sortedNums[x],
+                                sortedNums[y]
+                            });
+                        }
+                    }
+
+            foreach (List<int> value in uniqueResult.Values)
+                result.Add(value);
+
+            return result;
+        }
+    }
+}
---
+++ TDD_Practice.Test/ThreeSumTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDD_Practice.Test
+{
+    internal class ThreeSumTest
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/3sum/
+        /// Yet to resolve, failing reason is time exceeded
+        /// </summary>
+        [Test]
+        public void GIVEN_number_array___WHEN_three_sum___THEN_return_array_with_sum_in_zero()
+        {
+            AssertThreeSumAreEqual(GenerateArray(0, 1, 1), GenerateList());
+            AssertThreeSumAreEqual(GenerateArray(0, 0, 0), GenerateList(0, 0, 0));
+            AssertThreeSumAreEqual(GenerateArray(0, -1, 1), GenerateList(0, -1, 1));
+            AssertThreeSumAreEqual(GenerateArray(0, -1, 2), GenerateList());
+            AssertThreeSumAreEqual(GenerateArray(0, -1, -1, 2), GenerateList(-1, -1, 2));
+            var list = GenerateList(-1, -1, 2);
+            list.Add(GenerateList(-1, 0, 1)[0]);
+            AssertThreeSumAreEqual(GenerateArray(-1, 0, 1, 2, -1, -4), list);
+        }
+
+        private static IList<IList<int>> GenerateList(params int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return new List<IList<int>>();
+            }
+            else
+            {
+                return new List<IList<int>>
+                {
+                    numbers.OrderBy(n => n).ToList()
+                };
+            }
+        }
+
+        private static int[] GenerateArray(params int[] numbers)
+        {
+            return numbers;
+        }
+
+        private static void AssertThreeSumAreEqual(int[] targetArray, IList<IList<int>> expectedResult)
+        {
+            CollectionAssert.AreEqual(expectedResult, ThreeSumHelper.ThreeSum(targetArray));
+        }
+    }
+}
